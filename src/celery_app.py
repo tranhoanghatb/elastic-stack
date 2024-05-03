d862c889fee73e47400342278cc2e626cdd799bb
kitahara-saneyuki <edwarddwarf1999@gmail.com>
2024-05-03 20:59:27 +0800

    celery

--- src/celery_app.py
+++ src/celery_app.py
@@ -5,8 +5,8 @@ from celery import Celery
 celery_app = Celery(
     "tasks",
     broker="amqp://{rabbitmq_login}:{rabbitmq_pass}@{rabbitmq_host}//".format(
-        rabbitmq_login=os.environ["RABBITMQ_LOGIN"],
-        rabbitmq_pass=os.environ["RABBITMQ_PASS"],
+        rabbitmq_login="guest",
+        rabbitmq_pass="guest",
         rabbitmq_host=os.environ["RABBITMQ_HOST"],
     ),
     include=["apps.crawler.tasks"],
